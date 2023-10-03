d1223822d693b244522021e9ed6d37e77cd3c160
JUAN DE LOS SANTOS <j3delossantos@gmail.com>
2023-10-02 23:14:36 -0400

    Implemented KISS

--- Program.cs
+++ Program.cs
@@ -14,19 +14,19 @@ namespace ToDo
             do
             {
                 activeMenu = ShowMainMenu();
-                if (activeMenu == 1)
+                if ((Menu)activeMenu == Menu.Add)
                 {
                     ShowMenuAdd();
                 }
-                else if (activeMenu == 2)
+                else if ((Menu)activeMenu == Menu.Remove)
                 {
                     ShowMenuDelete();
                 }
-                else if (activeMenu == 3)
+                else if ((Menu)activeMenu == Menu.List)
                 {
                     ShowMenuTaskList();
                 }
-            } while (activeMenu != 4);
+            } while ((Menu)activeMenu != Menu.Exit);
         }
         /// <summary>
         /// Show the main menu 
@@ -34,12 +34,13 @@ namespace ToDo
         /// <returns>Returns option indicated by user</returns>
         public static int ShowMainMenu()
         {
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("Ingrese la opción a realizar: ");
-            Console.WriteLine("1. Nueva tarea");
-            Console.WriteLine("2. Remover tarea");
-            Console.WriteLine("3. Tareas pendientes");
-            Console.WriteLine("4. Salir");
+            Console.WriteLine($"----------------------------------------\n" +
+                $"Ingrese la opción a realizar: \n" +
+                $"1. Nueva tarea \n" +
+                $"2. Remover tarea\n" +
+                $"3. Tareas pendientes\n" +
+                $"2. 4. Salir");
+           
 
             // Read line
             string UserInput = Console.ReadLine();
@@ -52,23 +53,16 @@ namespace ToDo
             {
                 Console.WriteLine("Ingrese el número de la tarea a remover: ");
                 // Show current taks
-                for (int i = 0; i < TaskList.Count; i++)
-                {
-                    Console.WriteLine((i + 1) + ". " + TaskList[i]);
-                }
-                Console.WriteLine("----------------------------------------");
+                ShowMenuTaskList();
 
                 string UserInput = Console.ReadLine();
                 // Remove one position
                 int indexToRemove = Convert.ToInt32(UserInput) - 1;
-                if (indexToRemove > -1)
+                if (indexToRemove > -1 && TaskList.Count > 0)
                 {
-                    if (TaskList.Count > 0)
-                    {
                         string taskToDelete = TaskList[indexToRemove];
                         TaskList.RemoveAt(indexToRemove);
-                        Console.WriteLine("Tarea " + taskToDelete + " eliminada");
-                    }
+                        Console.WriteLine("Tarea " + taskToDelete + " eliminada");                   
                 }
             }
             catch (Exception)
@@ -99,12 +93,28 @@ namespace ToDo
             else
             {
                 Console.WriteLine("----------------------------------------");
+                int indexTask = 1;
+                TaskList.ForEach(p=> Console.WriteLine($"{indexTask++}. {p}"));
+
+                Console.WriteLine("----------------------------------------");
+
+                /*
                 for (int i = 0; i < TaskList.Count; i++)
                 {
                     Console.WriteLine((i + 1) + ". " + TaskList[i]);
                 }
-                Console.WriteLine("----------------------------------------");
+                */
+
             }
         }
+
+    }
+
+    public enum Menu
+    {
+        Add = 1,
+        Remove = 2,
+        List = 3,
+        Exit = 4
     }
 }
